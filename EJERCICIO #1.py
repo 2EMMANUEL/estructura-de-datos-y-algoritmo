c5fac0d858c9faad054c327f7e6ea5621a3c5972
Emmanuel <alcantaraemmanuel73@gmail.com>
2025-04-12 15:47:37 -0400

    update

---
+++ EJERCICIO #1.py
@@ -0,0 +1,5 @@
+#1.	Calcula el área de un rectángulo. Pide al usuario que ingrese el largo y el ancho, y luego muestra el área.#
+largo = int (input ("FAVOR DE INGRESAR EL EL LARGO "))
+ancho = int (input("FAVOR DE INGRESAR EL ANCHO "))
+area = (largo * ancho)
+print ("EL AREA DEL RECTANGULO ES " , area)
