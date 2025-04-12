5a6d32765d47965c701da10a51bf483999b4bbc7
Emmanuel <alcantaraemmanuel73@gmail.com>
2025-04-12 15:35:55 -0400

    update

--- PRUEBA.py
+++
@@ -1,9 +0,0 @@
-mi_lista=[10,20,30,40,50]
-sum=0 #acumulador de la suma de los elementos
-x=0 # contador de los elementos
- 
-while x<len(mi_lista):
-    sum+=mi_lista[x]
-    x+=1
-print (mi_lista)
-print (sum)
