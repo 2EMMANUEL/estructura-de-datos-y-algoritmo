d6603c776a07544914901e5fec6adc54c7f246aa
Emmanuel <alcantaraemmanuel73@gmail.com>
2025-04-12 17:02:05 -0400

    update

---
+++ EJERCICIO #7.py
@@ -0,0 +1,11 @@
+#7.	Escribe un programa que pida al usuario un número y determine si es positivo, negativo o cero.#
+
+N1 = int(input("INGRESE EL NUMERO "))
+if N1 == 0 : 
+ print("EL NUMERO ES CERO")
+elif N1 > 0 :
+ print("EL NUMERO ES POSITIVO")
+else :
+ print("EL NUMERO ES NEGATIVO")
+
+ 
